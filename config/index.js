6e33e39470fe12bea5299988c5f8b723d7040197
Anand Dharne <anand.dharne24@gmail.com>
2018-01-08 22:59:21 -0500

    first push

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/undiscovered/dist/',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
